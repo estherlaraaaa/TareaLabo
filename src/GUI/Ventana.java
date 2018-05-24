ad234e17b5639566247971e45c03650f15a29c43
estherlaraaaa <37164243+estherlaraaaa@users.noreply.github.com>
2018-05-23 19:54:52 -0600

    cambiando posicion de botones

--- src/GUI/Ventana.java
+++ src/GUI/Ventana.java
@@ -22,32 +22,32 @@ public class Ventana extends JPanel {
     public int WIDTH =445,widthTF=120, widthB =100;
     public int HEIGHT = 300, heightTF = 30, heightB= 30;
     public JTextField textF1, textF2, textF3;
-    public JButton sum,res,div,mult,bin,dec,oct,hex;
+    public JButton sum,res,div,mult,bin;
     
     public Ventana(){
         textF1 = new JTextField();
-        textF1.setBounds(new Rectangle(75,40,widthTF,heightTF));
+        textF1.setBounds(new Rectangle(85,40,widthTF,heightTF));
         
         textF2 = new JTextField();
-        textF2.setBounds(new Rectangle(225,40,widthTF,heightTF));
+        textF2.setBounds(new Rectangle(235,40,widthTF,heightTF));
         
         textF3 = new JTextField();
-        textF3.setBounds(new Rectangle(150,230,widthTF,heightTF));
+        textF3.setBounds(new Rectangle(160,230,widthTF,heightTF));
         
         sum = new JButton("+");
-        sum.setBounds(new Rectangle(20,115,widthB,heightB));
+        sum.setBounds(new Rectangle(70,115,widthB,heightB));
         
         res = new JButton("-");
-        res.setBounds(new Rectangle(120,115,widthB,heightB));
+        res.setBounds(new Rectangle(170,115,widthB,heightB));
         
         mult = new JButton("*");
-        mult.setBounds(new Rectangle(220,115,widthB,heightB));
+        mult.setBounds(new Rectangle(270,115,widthB,heightB));
         
         div = new JButton("/");
-        div.setBounds(new Rectangle(320,115,widthB,heightB));
+        div.setBounds(new Rectangle(220,150,widthB,heightB));
         
         bin = new JButton("Binario");
-        bin.setBounds(new Rectangle(20,150,widthB,heightB));
+        bin.setBounds(new Rectangle(120,150,widthB,heightB));
         
         
         textF1.setEditable(true);
